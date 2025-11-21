d30cf9ad30699134a3d4991a1708a4e24ab6a409
Leon Tran <leon_tran@sluz.ch>
2025-11-21 12:11:12 +0100

    go-1 Exercises done

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,14 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	var firstName string = "Leon"
+	var lastName string = "Tran"
+	var dayOfBirth int = 26
+	var monthOfBirth int = 8
+	var yearOfBirth int = 2008
+	var numberOfSiblings int = 3
+	var heightInMeters float32 = 1.72
+	var zodiacSign byte = 'V'
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
