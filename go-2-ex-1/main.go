66eaebd7879f83bd2eba4bad8690a4c316e2019f
Leon Tran <leon_tran@sluz.ch>
2025-11-21 12:45:18 +0100

    go-2 Exercises done

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -3,26 +3,40 @@ package main
 import "fmt"
 
 type FullName struct {
-	// TODO: add fields
+	FirstName string
+	LastName  string
 }
 
-// TODO: declare a structure for birth date
+type BirthDate struct {
+	Day   byte
+	Month byte
+	Year  int16
+}
 
 type Profile struct {
-	// TODO: embed full name and birth date information
+	Name             FullName
+	Born             BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
 
 func main() {
 	var me = Profile{
-		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		Name: FullName{
+			FirstName: "Leon",
+			LastName:  "Tran",
+		},
+		Born: BirthDate{
+			Day:   26,
+			Month: 8,
+			Year:  2008,
+		},
+		NumberOfSiblings: 3,
+		ZodiacSign:       'V',
 	}
 	fmt.Println(me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
-	// TODO: imagine, you get a little brother or sister
+	me.NumberOfSiblings += 1
 	fmt.Println("Siblings After:", me.NumberOfSiblings)
 }
--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -1,8 +1,28 @@
 package main
 
+import "fmt"
+
 func main() {
-	// TODO: declare a type for Student (with first and last name)
-	// TODO: declare a type for Class (consisting of multiple students)
-	// TODO: declare a map of modules being attended by multiple classes
-	// TODO: output everything using fmt.Println()
+	type Student struct {
+		FirstName string
+		LastName  string
+	}
+	type Class struct {
+		Students []Student
+	}
+	modules := map[int]Class{
+		1: {
+			Students: []Student{
+				{FirstName: "Leon", LastName: "Tran"},
+				{FirstName: "Alina", LastName: "Fischer"},
+			},
+		},
+		2: {
+			Students: []Student{
+				{FirstName: "Nevin", LastName: "Wyss"},
+				{FirstName: "Max", LastName: "Mustermann"},
+			},
+		},
+	}
+	fmt.Println(modules)
 }
