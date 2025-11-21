e9535fd0a179770da02d6742737b0f4019f3d361
Leon Tran <leon_tran@sluz.ch>
2025-11-21 12:59:07 +0100

    go-3 ex-1 done

--- go-3-ex-1/main.go
+++ go-3-ex-1/main.go
@@ -19,8 +19,33 @@ const (
 
 func outputWithZodiacSign(p Person) {
 	var zodiacSign rune = '?'
-
-	// TODO: Assign proper value to zodiacSign using if/else branching.
+	if p.BirthDate.Day >= 21 && p.BirthDate.Month == 3 || p.BirthDate.Day <= 20 && p.BirthDate.Month == 4 {
+		zodiacSign = Aries
+	} else if p.BirthDate.Day >= 21 && p.BirthDate.Month == 4 || p.BirthDate.Day <= 21 && p.BirthDate.Month == 5 {
+		zodiacSign = Taurus
+	} else if p.BirthDate.Day >= 22 && p.BirthDate.Month == 5 || p.BirthDate.Day <= 21 && p.BirthDate.Month == 6 {
+		zodiacSign = Gemini
+	} else if p.BirthDate.Day >= 22 && p.BirthDate.Month == 6 || p.BirthDate.Day <= 22 && p.BirthDate.Month == 7 {
+		zodiacSign = Cancer
+	} else if p.BirthDate.Day >= 23 && p.BirthDate.Month == 7 || p.BirthDate.Day <= 22 && p.BirthDate.Month == 8 {
+		zodiacSign = Leo
+	} else if p.BirthDate.Day >= 23 && p.BirthDate.Month == 8 || p.BirthDate.Day <= 22 && p.BirthDate.Month == 9 {
+		zodiacSign = Virgo
+	} else if p.BirthDate.Day >= 23 && p.BirthDate.Month == 9 || p.BirthDate.Day <= 22 && p.BirthDate.Month == 10 {
+		zodiacSign = Libra
+	} else if p.BirthDate.Day >= 23 && p.BirthDate.Month == 10 || p.BirthDate.Day <= 22 && p.BirthDate.Month == 11 {
+		zodiacSign = Scorpius
+	} else if p.BirthDate.Day >= 23 && p.BirthDate.Month == 11 || p.BirthDate.Day <= 20 && p.BirthDate.Month == 12 {
+		zodiacSign = Sagittarius
+	} else if p.BirthDate.Day >= 21 && p.BirthDate.Month == 12 || p.BirthDate.Day <= 19 && p.BirthDate.Month == 1 {
+		zodiacSign = Capricornus
+	} else if p.BirthDate.Day >= 20 && p.BirthDate.Month == 1 || p.BirthDate.Day <= 18 && p.BirthDate.Month == 2 {
+		zodiacSign = Aquarius
+	} else if p.BirthDate.Day >= 19 && p.BirthDate.Month == 2 || p.BirthDate.Day <= 20 && p.BirthDate.Month == 3 {
+		zodiacSign = Pisces
+	} else {
+		zodiacSign = '?'
+	}
 	// NOTE: The runes are defined above as constants.
 
 	fmt.Printf("%s %s, born on %02d.%02d.%04d, has the zodiac sign %c.\n",
@@ -54,4 +79,12 @@ func main() {
 	outputWithZodiacSign(rick)
 	outputWithZodiacSign(edsger)
 	outputWithZodiacSign(alan)
+
+	leon := Person{FullName{"Leon", "Tran"}, BirthDate{26, 8, 2008}}
+	alina := Person{FullName{"Alina", "Fischer"}, BirthDate{20, 11, 2007}}
+	sophie := Person{FullName{"Sophie", "Tran"}, BirthDate{31, 5, 2010}}
+
+	outputWithZodiacSign(leon)
+	outputWithZodiacSign(alina)
+	outputWithZodiacSign(sophie)
 }
